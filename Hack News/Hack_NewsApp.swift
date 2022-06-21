b7eea0ef4b86a2c6566a6358eeec3b7dbee9e9fd
ana <ani_dzamelashvili@yahoo.com>
2022-06-21 15:50:25 +0400

    Initial Commit

---
+++ Hack News/Hack_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hack_NewsApp.swift
+//  Hack News
+//
+//  Created by Ana Dzamelashvili on 6/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Hack_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
